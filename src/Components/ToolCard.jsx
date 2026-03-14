6bbd2a47fc36c713a83e950acc83764f6328f861
robalexg <alexg7070@yahoo.com>
2026-03-14 14:27:11 -0500

    fixed card

--- src/Components/ToolCard.jsx
+++ src/Components/ToolCard.jsx
@@ -4,12 +4,12 @@ const ToolCard = ({tool}) => {
 
     return(
         <div className='tool-card'>
+            <div className='tool-img-container'>
+                <img src={`.${tool.image}`} alt="" />
+            </div>
             <div>
                 <h1>{tool.tool}</h1>
                 <h2>{tool.name}</h2>
-            </div>
-            <div className='tool-img-container'>
-                <img src={`.${tool.image}`} alt="" />
             </div>
             <h3>Location: {tool.location}</h3>
         </div>
